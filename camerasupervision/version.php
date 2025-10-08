c2fb6ffb558b4900f2a50f94edd2c60b11bcc3f9
davidbarriosone <148712800+davidbarriosone@users.noreply.github.com>
2025-10-08 17:55:23 -0300

    Update version.php

--- camerasupervision/version.php
+++ camerasupervision/version.php
@@ -5,4 +5,4 @@ $plugin->component = 'quizaccess_camerasupervision';
 $plugin->version   = 2025081806; // Versión actualizada con reconocimiento facial
 $plugin->requires  = 2020110900; // Moodle 3.11.
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.3';`
+$plugin->release   = '0.3';
